dd05c2846ff5fd53a79bdd35e12e929de9455b4c
dps08 <singh.divi@northeastern.edu>
2024-12-14 04:27:22 +0530

    final commit

--- index.js
+++ index.js
@@ -1,44 +1,52 @@
-import express from 'express';
-import Hello from './hello.js';
-import Lab5 from './Lab5/index.js';
-import UserRoutes from './Kanbas/Users/routes.js';
-import CourseRoutes from './Kanbas/Courses/routes.js';
-import cors from "cors";
-import session from "express-session";
-import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import "dotenv/config";
-import AssignmentRoutes from './Kanbas/Assignments/routes.js';
+import express from 'express';
+import Hello from './Hello.js';
+import Lab5 from './Lab5/index.js';
+import cors from 'cors';
+import session from "express-session";
+import UserRoutes from "./Kanbas/Users/routes.js";
+import CourseRoutes from "./Kanbas/Courses/routes.js";
+import ModuleRoutes from "./Kanbas/Modules/routes.js";
+import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
 import mongoose from "mongoose";
+
 const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
 mongoose.connect(CONNECTION_STRING);
+
 const app = express();
-app.use(
-    cors({
-        credentials: true,
-        origin: process.env.NETLIFY_URL || "http://localhost:3000",
-    })
-);
+
+app.use(cors({
+    credentials: true,
+    origin: [
+        process.env.NETLIFY_URL || "http://localhost:3000", // Update for frontend domain
+    ],
+}));
+
 const sessionOptions = {
     secret: process.env.SESSION_SECRET || "kanbas",
     resave: false,
     saveUninitialized: false,
 };
+
 if (process.env.NODE_ENV !== "development") {
     sessionOptions.proxy = true;
     sessionOptions.cookie = {
         sameSite: "none",
         secure: true,
-        domain: process.env.NODE_SERVER_DOMAIN,
+        httpOnly: true,
     };
 }
+
 app.use(session(sessionOptions));
-
-
 app.use(express.json());
-Lab5(app);
-Hello(app);
+
 UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
-app.listen(process.env.PORT || 4000)
+Hello(app);
+Lab5(app);
+
+app.listen(process.env.PORT || 4000, () => {
+    console.log("Server running on port", process.env.PORT || 4000);
+});
