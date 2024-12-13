4cb65b775397b5fb85e0684d77b6d0f685e335f4
dps08 <singh.divi@northeastern.edu>
2024-12-14 04:30:11 +0530

    final commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import express from 'express';
-import Hello from './Hello.js';
+import Hello from './hello.js';
 import Lab5 from './Lab5/index.js';
 import cors from 'cors';
 import session from "express-session";
